628f45110264fa00170d35e35e9beae9dd12a586
BabuTeis <huizingastijn@gmail.com>
2025-01-24 01:48:32 +0100

    Updated page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,16 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is a machine learning app, made by BabuTeis.')
 
-# df = dataframe
-df = pd.read_csv('dataset_folder/penguins_cleaned.csv')
-df 
+with st.expander('Data'):
+    st.write('**Raw data**')
+    
+    df = pd.read_csv('dataset_folder/penguins_cleaned.csv')
+    df # Prints entire dataframe.
+    
+    st.write('**X**')
+    X = df.drop('species', axis=1)
+    X
+    
+    st.write('**y**')
+    y = df.species
+    y
