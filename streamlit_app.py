ecb78eed26519259425a6825f63778a749561293
BabuTeis <huizingastijn@gmail.com>
2025-01-24 15:45:14 +0100

    Updated page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is a machine learning app, made by BabuTeis.')
 
-st.text('This website is an interactive machine learning app designed '
-        'to classify penguin species based on user-provided features '
+st.text('This website is an **interactive machine learning app designed '
+        'to classify penguin species** based on user-provided features '
         'such as island, bill length, bill depth, flipper length, body '
         'mass, and gender. Users can input these features using intuitive '
         'sliders and dropdowns, and the app predicts the penguin species '
