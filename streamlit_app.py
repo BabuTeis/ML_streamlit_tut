4684e0b6b2ea675efefc3a67b1ef55c5cde1dae2
BabuTeis <huizingastijn@gmail.com>
2025-01-24 01:38:50 +0100

    Update pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This is a machine learning app, made by BabuTeis.')
+
+# df = dataframe
+df = pd.read_csv('dataset_folder/penguins_cleaned.csv')
+df 
